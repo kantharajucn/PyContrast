9a74b083f842e0d4faf24e18c6995fc230282b08
kantharajucn <kantharajucnQoutlook.com>
2020-07-24 13:08:51 +0200

    loading the weights correctly

--- hubconf.py
+++ hubconf.py
@@ -1,9 +1,44 @@
+from collections import OrderedDict
+
 import torch.nn as nn
 import torch
 
 
 dependencies = ['torch']
 
+
+def _load_encoder_weights(model, modal, state):
+    """load pre-trained weights for encoder
+
+    Args:
+      model: pretrained encoder, should be frozen
+    """
+    if modal == 'RGB':
+        # Unimodal (RGB) case
+        encoder_state_dict = OrderedDict()
+        for k, v in state.items():
+            k = k.replace('module.', '')
+            if 'encoder' in k:
+                k = k.replace('encoder.', '')
+                encoder_state_dict[k] = v
+        model.encoder.load_state_dict(encoder_state_dict)
+    else:
+        # Multimodal (CMC) case
+        encoder1_state_dict = OrderedDict()
+        encoder2_state_dict = OrderedDict()
+        for k, v in state.items():
+            k = k.replace('module.', '')
+            if 'encoder1' in k:
+                k = k.replace('encoder1.', '')
+                encoder1_state_dict[k] = v
+            if 'encoder2' in k:
+                k = k.replace('encoder2.', '')
+                encoder2_state_dict[k] = v
+        model.encoder1.load_state_dict(encoder1_state_dict)
+        model.encoder2.load_state_dict(encoder2_state_dict)
+    print('Pre-trained weights loaded!')
+
+    return model
 
 
 def InsDis(pretrained=False, **kwargs):
@@ -18,11 +53,10 @@ def InsDis(pretrained=False, **kwargs):
     model = RGBSingleHead(*kwargs)
 
     if pretrained:
-        model = nn.DataParallel(model)
         url = 'https://www.dropbox.com/sh/87d24jqsl6ra7t2/AACcsSIt1_Njv7GsmsuzZ6Sta/InsDis.pth?dl=1'
-        state_dict = torch.hub.load_state_dict_from_url(url)
-        print(state_dict.keys())
-        model.load_state_dict(state_dict["model"])
+        device = torch.device("cpu")
+        state_dict = torch.hub.load_state_dict_from_url(url, map_location=device)
+        model = _load_encoder_weights(model, modal="RGB", state=state_dict["model"])
     return model
 
 def CMC(pretrained=False, **kwargs):
@@ -39,9 +73,9 @@ def CMC(pretrained=False, **kwargs):
     if pretrained:
         model = nn.DataParallel(model)
         url = 'https://www.dropbox.com/sh/87d24jqsl6ra7t2/AACYcqgM-lcG3__QIbxuM2Koa/CMC.pth?dl=1'
-        state_dict = torch.hub.load_state_dict_from_url(url)
-        print(state_dict.keys())
-        model.load_state_dict(state_dict["model"])
+        device = torch.device("cpu")
+        state_dict = torch.hub.load_state_dict_from_url(url, map_location=device)
+        model = _load_encoder_weights(model, modal="CMC", state=state_dict["model"])
     return model
 
 def MoCo(pretrained=False, **kwargs):
@@ -58,9 +92,9 @@ def MoCo(pretrained=False, **kwargs):
     if pretrained:
         model = nn.DataParallel(model)
         url = 'https://www.dropbox.com/sh/87d24jqsl6ra7t2/AAB53yJAYuCrOFluygBsVKOOa/MoCo.pth?dl=1'
-        state_dict = torch.hub.load_state_dict_from_url(url)
-        print(state_dict.keys())
-        model.load_state_dict(state_dict["model"])
+        device = torch.device("cpu")
+        state_dict = torch.hub.load_state_dict_from_url(url, map_location=device)
+        model = _load_encoder_weights(model, modal="RGB", state=state_dict["model"])
     return model
 
 
@@ -78,9 +112,9 @@ def MoCoV2(pretrained=False, **kwargs):
     if pretrained:
         model = nn.DataParallel(model)
         url = 'https://www.dropbox.com/sh/87d24jqsl6ra7t2/AABaYuKiiZFYowa31yKeGGOQa/MoCov2.pth?dl=1'
-        state_dict = torch.hub.load_state_dict_from_url(url)
-        print(state_dict.keys())
-        model.load_state_dict(state_dict["model_ema"])
+        device = torch.device("cpu")
+        state_dict = torch.hub.load_state_dict_from_url(url, map_location=device)
+        model = _load_encoder_weights(model, modal="RGB", state=state_dict["model"])
     return model
 
 def PIRL(pretrained=False, **kwargs):
@@ -97,9 +131,9 @@ def PIRL(pretrained=False, **kwargs):
     if pretrained:
         model = nn.DataParallel(model)
         url = 'https://www.dropbox.com/sh/87d24jqsl6ra7t2/AADN4jKnvTI0U5oT6hTmQZz8a/PIRL.pth?dl=1'
-        state_dict = torch.hub.load_state_dict_from_url(url)
-        print(state_dict.keys())
-        model.load_state_dict(state_dict["model"])
+        device = torch.device("cpu")
+        state_dict = torch.hub.load_state_dict_from_url(url, map_location=device)
+        model = _load_encoder_weights(model, modal="RGB", state=state_dict["model"])
     return model
 
 def InfoMin(pretrained=False, **kwargs):
